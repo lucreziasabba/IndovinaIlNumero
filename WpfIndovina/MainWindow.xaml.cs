4a61b5e4b139b9bb59e531ffa12e3a43df5fb8c4
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-09 09:51:38 +0100

    Add project files.

---
+++ WpfIndovina/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfIndovina
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random random_generator = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Genera_Click(object sender, RoutedEventArgs e)
+        {
+            int livello = int.Parse(txtLivello.Text);
+            int RandomNumber = random_generator.Next(1, livello);
+            int numero = int.Parse(txtNumero.Text);
+            if (numero == RandomNumber)
+                txtNumEstratto.Text = ($"Il numero estratto è {RandomNumber}. Complimenti!Hai vinto!");
+            else if (numero < RandomNumber)
+                txtNumEstratto.Text = ($"Il numero estratto è più grande di quello che hai inserito!");
+            else if (numero > RandomNumber)
+                txtNumEstratto.Text = ($"Il numero estratto è più piccolo di quello che hai inserito!");
+        }
+
+        private void Ricomincia_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumEstratto.Clear();
+            txtNumero.Clear();
+            txtLivello.Clear();
+        }
+    }
+}
